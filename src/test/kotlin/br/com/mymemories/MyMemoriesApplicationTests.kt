198efaa362b41526f5e08f9a5e09be1567061d91
Gabriel da Silva <gabriel.dsilva@sptech.school>
2023-01-30 13:50:25 -0300

    Added annotation springboottest

--- src/test/kotlin/br/com/mymemories/MyMemoriesApplicationTests.kt
+++ src/test/kotlin/br/com/mymemories/MyMemoriesApplicationTests.kt
@@ -3,10 +3,4 @@ package br.com.mymemories
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
-class MyMemoriesApplicationTests {
-
-    @Test
-    fun contextLoads() {
-    }
-}
+class MyMemoriesApplicationTests
