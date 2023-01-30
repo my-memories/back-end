7d0f36fc7233b400e26116de140551bac644bdbc
Gabriel da Silva <gabriel.dsilva@sptech.school>
2023-01-30 02:35:18 -0300

    :construction: Jenkins configuration

---
+++ src/test/kotlin/br/com/mymemories/domain/UserTest.kt
@@ -0,0 +1,16 @@
+package br.com.mymemories.domain
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UserTest {
+
+    @Test
+    fun testInstanceOfUser() {
+        val user = User()
+
+        Assertions.assertNotNull(user)
+    }
+}
