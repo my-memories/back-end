d559aaed705a2fb9cd027a30bcb8e2f4b7f69c69
Gabriel da Silva <gabriel.dsilva@sptech.school>
2023-01-30 13:43:34 -0300

    Added annotation springboottest

--- src/test/kotlin/br/com/mymemories/domain/UserTest.kt
+++ src/test/kotlin/br/com/mymemories/domain/UserTest.kt
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
+@SpringBootTest
 class UserTest {
 
     @Test
